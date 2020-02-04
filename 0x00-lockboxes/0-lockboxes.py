0e3d273d732a12f00fe6a34f4a034c1bfbe3e3ab
Arthur Damm <indigoarthur@gmail.com>
2020-02-03 16:32:43 -0800

    add 0-lockboxes.py

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -9,14 +9,15 @@ def canUnlockAll(boxes):
     if not len(boxes):
         return True
     visited = [False for i in range(len(boxes))]
+    visited[0] = True
     stack = [0]
     while len(stack):
         box = stack.pop(0)
-        visited[box] = True
         if not isinstance(boxes[box], list):
             return False
         for _box in boxes[box]:
             if isinstance(_box, int) and _box >= 0 and _box < len(boxes)\
               and not visited[_box]:
+                visited[_box] = True
                 stack.append(_box)
     return all(visited)
