73459194eed87d937293b2990a6d2f06f15a4dc4
Arthur Damm <indigoarthur@gmail.com>
2020-02-28 06:14:16 +0000

    Add sandpiles algorithm

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,104 @@
+#include "sandpiles.h"
+
+/**
+ * sandpiles_sum - computes the final stable sum of two sandpiles
+ * @grid1: the first sandpile
+ * @grid2: the second sandpile
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+	size_t i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] += grid2[i][j];
+			grid2[i][j] = 0;
+		}
+	}
+	while (is_unstable(grid1))
+	{
+		print_sandpile(grid1);
+		topple_sandpile(grid1, grid2);
+	}
+
+}
+
+/**
+ * topple_sandpile - topples a sandpile, dispersing all cells > 3
+ * @grid1: the sandpile to topple
+ * @grid2: the 'buffer' pile which stores the dispersions
+ */
+void topple_sandpile(int grid1[3][3], int grid2[3][3])
+{
+	size_t i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid1[i][j] > 3)
+			{
+				if (i > 0)
+					grid2[i - 1][j] += 1;
+				if (j > 0)
+					grid2[i][j - 1] += 1;
+				if (i < 2)
+					grid2[i + 1][j] += 1;
+				if (j < 2)
+					grid2[i][j + 1] += 1;
+				grid1[i][j] -= 4;
+			}
+		}
+	}
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] += grid2[i][j];
+			grid2[i][j] = 0;
+		}
+	}
+}
+
+/**
+ * is_unstable - checks if sandpile is unstable
+ * @grid: the sandpile to check
+ * Return: 1 if unstable else 0
+ */
+int is_unstable(int grid[3][3])
+{
+	size_t i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+				return (1);
+		}
+	}
+	return (0);
+}
+
+/**
+ * print_sandpile - prints the sandpile
+ * @grid: the sandpile to print
+ */
+void print_sandpile(int grid[3][3])
+{
+	int i, j;
+
+	printf("=\n");
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
+}
---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,12 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+#include "stddef.h"
+#include "stdio.h"
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+int is_unstable(int grid[3][3]);
+void topple_sandpile(int grid1[3][3], int grid2[3][3]);
+void print_sandpile(int grid[3][3]);
+
+#endif
