d3a1ab5072cb85708fce2f196f574f3d55b3a3d6
Arthur Damm <indigoarthur@gmail.com>
2020-03-01 16:10:37 +0000

    add sandpiles.h

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -5,6 +5,7 @@
 #include "stdio.h"
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void add_sandpile(int grid1[3][3], int grid2[3][3]);
 int is_unstable(int grid[3][3]);
 void topple_sandpile(int grid1[3][3], int grid2[3][3]);
 void print_sandpile(int grid[3][3]);
