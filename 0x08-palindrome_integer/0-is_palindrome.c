79c9ddd86f4f8149e8c04771500815907cfa87a5
tommy <indigoarthur@gmail.com>
2020-03-30 18:24:57 +0000

    Use static var even better

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -1,16 +1,6 @@
 #include "palindrome.h"
 
-/**
- * is_palindrome_recursive - determines if ulong is palindrome
- * @n: the right-shifted digits of ulong to check
- * @_n: address of copy of ulong to check against in reverse
- * Return: 1 if palindrome else 0
- */
-int is_palindrome_recursive(unsigned long n, unsigned long *_n)
-{
-	return (!n || (is_palindrome_recursive(n / 10, _n)
-		 && (n % 10 == *_n % 10) ? (*_n /= 10), 1 : 0));
-}
+static unsigned long _n;
 
 /**
  * is_palindrome - determines if ulong is palindrome
@@ -19,5 +9,6 @@ int is_palindrome_recursive(unsigned long n, unsigned long *_n)
  */
 int is_palindrome(unsigned long n)
 {
-	return (is_palindrome_recursive(n, &n));
+	return (((!_n ? _n = n : 0), !n) || (is_palindrome(n / 10)
+		&& (n % 10 == _n % 10) ? (_n /= 10), 1 : 0));
 }
