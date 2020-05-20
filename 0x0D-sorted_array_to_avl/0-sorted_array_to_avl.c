8caa37212c1a99a73b4d8c4a59d9f594b7a96211
tommy <indigoarthur@gmail.com>
2020-05-20 17:30:51 +0000

    Beautify some more

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -11,24 +11,18 @@
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
 	size_t mid;
-	avl_t *parent, *left, *right = NULL;
+	avl_t *parent;
 
-	if (!array || !size)
+	if (!array || size < 1)
 		return (NULL);
 	parent = calloc(1, sizeof(avl_t));
 	if (!parent)
 		return (NULL);
 	mid = size / 2;
 	parent->n = array[mid];
-	if (size == 1)
-		return (parent);
-	left = sorted_array_to_avl(array, mid);
-	if (mid + 1 < size)
-		right = sorted_array_to_avl(array + mid + 1, size - mid - 1);
-	parent->left = left;
-	left->parent = parent;
-	parent->right = right;
-	if (right)
-		right->parent = parent;
+	parent->left = sorted_array_to_avl(array, mid);
+	parent->right = sorted_array_to_avl(array + mid + 1, size - mid - 1);
+	parent->left ? parent->left->parent = parent : 0;
+	parent->right ? parent->right->parent = parent : 0;
 	return (parent);
 }
