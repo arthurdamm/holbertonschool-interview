e175277a2b7f2b14361b04b11bb41a34c8cfc9e8
tommy <indigoarthur@gmail.com>
2020-05-27 23:41:53 +0000

    Add linear skip search

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,41 @@
+#include "search.h"
+
+/**
+ * linear_skip - search via linear skip
+ * @list: pointer to skip list
+ * @value: to search for
+ * Return: address of node or NULL
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *fast = list;
+
+	if (!list)
+		return (NULL);
+	while (fast->express)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+			fast->express->index, fast->express->n);
+		if (fast->express->n > value)
+		{
+			printf("Value found between indexes [%lu] and [%lu]\n",
+				fast->index, fast->express->index);
+			break;
+		}
+		fast = fast->express;
+	}
+	if (!fast->express)
+	{
+		for (list = fast; list->next; list = list->next)
+			;
+		printf("Value found between indexes [%lu] and [%lu]\n",
+				fast->index, list->index);
+	}
+	for (list = fast; list != fast->express; list = list->next)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
+		if (value == list->n)
+			break;
+	}
+	return (list == fast->express ? NULL : list);
+}
