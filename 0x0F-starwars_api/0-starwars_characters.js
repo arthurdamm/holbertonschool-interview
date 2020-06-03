07f1682dae2bc034c8f5e7b1815ee189d9de8e43
tommy <indigoarthur@gmail.com>
2020-06-03 15:45:49 -0700

    add 0-starwars_characters.js

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+const request = require('request');
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+request(url, function (error, response, body) {
+  if (error) {
+    return;
+  }
+  const json = JSON.parse(body);
+  const urlToName = {};
+  for (const characterUrl of json.characters) {
+    request(characterUrl, function (error, response, body) {
+      if (error) {
+        return;
+      }
+      const character = JSON.parse(body);
+      urlToName[character.url] = character.name;
+      if (Object.keys(urlToName).length === json.characters.length) {
+        for (const characterUrl of json.characters) {
+          console.log(urlToName[characterUrl]);
+        }
+      }
+    });
+  }
+});
