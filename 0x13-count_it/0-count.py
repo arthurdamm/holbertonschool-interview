eefe18e0f57d8a72cb507b15fc4bcea64e91495f
tommy <indigoarthur@gmail.com>
2020-08-21 02:42:28 +0000

    add 0-count.py

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''Get ALL hot posts'''
 import pprint
+import re
 import requests
 
 BASE_URL = 'http://reddit.com/r/{}/hot.json'
@@ -28,6 +29,7 @@ def count_words(subreddit, word_list, hot_list=[], after=None):
                            for post in data.get('children', [])]
     return count_words(subreddit, word_list, hot_list, after)
 
+
 def print_results(word_list, hot_list):
     '''Prints request results'''
     count = {}
@@ -35,9 +37,11 @@ def print_results(word_list, hot_list):
         count[word] = 0
     for title in hot_list:
         for word in word_list:
-            count[word] = count[word] + title.count(word)
+            count[word] = count[word] +\
+             len(re.findall(r'(?:^| ){}(?:$| )'.format(word), title, re.I))
 
     count = {k: v for k, v in count.items() if v > 0}
-    for word in sorted(list(count.keys()),
+    words = sorted(list(count.keys()))
+    for word in sorted(words,
                        reverse=True, key=lambda k: count[k]):
         print("{}: {}".format(word, count[word]))
