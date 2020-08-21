1c4dcd9c1a4a76a25f3cd32c945821e5b342b1ad
tommy <indigoarthur@gmail.com>
2020-08-21 02:48:15 +0000

    add 0-count.py

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -41,6 +41,7 @@ def print_results(word_list, hot_list):
              len(re.findall(r'(?:^| ){}(?:$| )'.format(word), title, re.I))
 
     count = {k: v for k, v in count.items() if v > 0}
+    words = sorted(list(count.keys()))
     for word in sorted(words,
                        reverse=True, key=lambda k: count[k]):
         print("{}: {}".format(word, count[word]))
