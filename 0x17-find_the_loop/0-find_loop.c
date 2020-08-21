88280096b4512e67100611874e158a71c72a564d
tommy <indigoarthur@gmail.com>
2020-08-21 04:13:18 +0000

    Add task 0-find_loop.c

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - uses floyd's algo to find loop
+ * @head: pointer to head node of list
+ * Return: pointer to node where loop starts
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *fast = head, *slow = head;
+
+	if (!head)
+		return (NULL);
+	while (fast && fast->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow)
+		{
+			while (fast && fast->next)
+			{
+				fast = fast->next->next;
+				slow = slow->next;
+				if (fast == slow)
+				{
+					return (slow);
+				}
+			}
+		}
+	}
+	return (NULL);
+}
