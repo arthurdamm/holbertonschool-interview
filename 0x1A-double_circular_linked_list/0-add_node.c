689f60a9a12e8e9157023927ef0c7ea0e000f94a
tommy <indigoarthur@gmail.com>
2020-09-02 19:02:48 +0000

    Simplify to take advantage of circularity

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -24,8 +24,7 @@ List *add_node_end(List **list, char *str)
 		new_node->prev = new_node->next = new_node;
 		return (*list = new_node);
 	}
-	for (node = *list; node->next != *list; node = node->next)
-		;
+	node = (*list)->prev;
 	new_node->next = *list;
 	node->next = new_node;
 	new_node->prev = node;
@@ -41,25 +40,5 @@ List *add_node_end(List **list, char *str)
  */
 List *add_node_begin(List **list, char *str)
 {
-	List *new_node = NULL;
-
-	if (!list)
-		return (NULL);
-	new_node = malloc(sizeof(*new_node));
-	if (!new_node)
-		return (NULL);
-	new_node->prev = new_node->next = NULL;
-	new_node->str = strdup(str);
-	if (str && !new_node->str)
-		return (free(new_node), NULL);
-	if (!*list)
-	{
-		new_node->prev = new_node->next = new_node;
-		return (*list = new_node);
-	}
-	new_node->prev = (*list)->prev;
-	(*list)->prev->next = new_node;
-	(*list)->prev = new_node;
-	new_node->next = (*list);
-	return (*list = new_node);
+	return (add_node_end(list, str) ? *list = (*list)->prev : NULL);
 }
