33b75323f2266b7fa1879d7d3aaf11f32947ed67
tommy <indigoarthur@gmail.com>
2020-09-14 23:10:24 +0000

    Copied this beautiful simplification

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -4,30 +4,15 @@ Island Perimeter Algo
 O(n) time O(1) space
 """
 
-
 def island_perimeter(grid):
-    """Finds water to call DFS method"""
-
-    def dfs(row, col):
-        """ Uses DFS to calculate permeter"""
-        if not 0 <= row < len(grid) or not 0 <= col < len(grid[row]) or \
-           grid[row][col] != 1:
-            return 0
-        grid[row][col] = -1
-        perimeter = 0
-        for dr, dc in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-            if not 0 <= row + dr < len(grid) or \
-               not 0 <= col + dc < len(grid[row]) or \
-               grid[row + dr][col + dc] == 0:
-                perimeter += 1
-        for dr, dc in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-            perimeter += dfs(row + dr, col + dc)
-        return perimeter
-
+    """Looks up and left to subtract water"""
     perimeter = 0
     for row in range(len(grid)):
         for col in range(len(grid[row])):
             if grid[row][col] == 1:
-                perimeter = dfs(row, col)
-                break
+                perimeter += 4
+                if row and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col and grid[row][col - 1] == 1:
+                    perimeter -= 2
     return perimeter
