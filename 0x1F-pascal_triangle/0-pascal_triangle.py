ea15c3a1220837bb66715390e43e0ebfd2dbd05a
tommy <indigoarthur@gmail.com>
2020-10-05 19:34:04 -0700

    Try docstring

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-# Computes N rows of Pascal's Triangle
+"""Computes N rows of Pascal's Triangle"""
 
 
 def pascal_triangle(n):
-    # Computes N rows of Pascal's Triangle
+    """Computes N rows of Pascal's Triangle"""
     pascal = [[1]]
     for _ in range(n - 1):
         pascal += [[1] + [pascal[-1][i] + pascal[-1][i + 1]
