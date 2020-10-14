96feb84d43504f02020ca331194a5c1f851ae1d4
tommy <indigoarthur@gmail.com>
2020-10-14 16:30:12 +0000

    Clean up

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -3,7 +3,7 @@
 /**
  * regex_match - matches regex pattern to string
  * @s: the string to search
- * @p: the pattern
+ * @p: the pattern to match
  * Return: 1 if match else 0
  */
 int regex_match(char const *s, char const *p)
@@ -11,8 +11,8 @@ int regex_match(char const *s, char const *p)
 	if (!*s)
 		return (!*p || (p[1] == '*' && regex_match(s, p + 2)));
 	if (p[1] == '*')
-		return ((p[0] == '.' || s[0] == p[0]) ?
-			(regex_match(s + 1, p) || regex_match(s, p + 2)) :
-			(regex_match(s, p + 2)));
-	return ((p[0] == '.' || p[0] == s[0]) && regex_match(s + 1, p + 1));
+		return ((*p == '.' || *s == *p)
+			 ? (regex_match(s + 1, p) || regex_match(s, p + 2))
+			 : (regex_match(s, p + 2)));
+	return ((*p == '.' || *p == *s) && regex_match(s + 1, p + 1));
 }
