581154b031d6b3a128583a8ecc773245a4c18ab9
tommy <indigoarthur@gmail.com>
2020-10-21 18:45:37 +0000

    Try using lib funcs

--- 0x21-substring/substring.c
+++ 0x21-substring/substring.c
@@ -1,5 +1,10 @@
 #include "substring.h"
 
+
+int cmp(const void *a, const void *b) {
+	return strcmp(a, b);
+}
+
 /**
  * find_substring - finds substring composed of all concatenated words
  * @s: the string to search
@@ -10,9 +15,19 @@
  */
 int *find_substring(char const *s, char const **words, int nb_words, int *n)
 {
+	int i = 0;
+
+	setbuf(stdout, NULL);
+	for (i = 0; i < nb_words; i++)
+		printf("[%d] [%s]\n", i, words[i]);
+	qsort(words, nb_words, sizeof(char *), cmp);
+	for (i = 0; i < nb_words; i++)
+		printf("[%d] [%s]\n", i, words[i]);
+	printf("<%s>\n", (char *)bsearch(words[2], words, nb_words, strlen(words[0]), cmp));
 	*n = 0;
 	return (NULL);
 	(void)s;
 	(void)words;
 	(void)nb_words;
 }
+
